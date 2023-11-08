4fe84eb3bea7da39aeba9f6b1c7b47c86ec020f5
Guilherme Dalla Rosa <guilherme.rosa@wecode.digital>
2023-11-08 14:57:03 -0300

    Add mixins padrões

--- src/pages/PaginaInicial/index.jsx
+++ src/pages/PaginaInicial/index.jsx
@@ -1,6 +1,15 @@
+import { Link } from "react-router-dom"
+import "./Mobile/sass/PaginaInicial.css"
+
 const PaginaInicial = () => {
     return (
-        <p>Pagina inicial</p>
+        <>
+            <h3><span className="text-highlight">Consultoria estratégica</span> para estruturar e acelerar sua operação de <span className="text-highlight">e-commerce e marketplace</span></h3>
+
+
+            <Link to="/quem-somos">
+                <button><p>Saiba Mais</p></button></Link>
+        </>
     )
 }
 
