3076ad4be56640793932f96c5cc88aff0c949544
Estevão Bresolin <estevao.bresolin@wecode.digital>
2023-11-10 16:04:16 -0300

    pagina nossos clientes

---
+++ src/pages/NossosClientes/Mobile/index.jsx
@@ -0,0 +1,12 @@
+import SessaoNossosClientesMobile from './SessaoNossosClientes'
+
+const NossosClientesMobile = () => {
+    return(
+        <div>
+        <SessaoNossosClientesMobile />
+    </div>
+    )
+    
+}
+
+export default NossosClientesMobile
