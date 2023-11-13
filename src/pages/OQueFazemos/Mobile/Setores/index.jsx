8ddfb083045cde4cbd9df056e7612180a912be23
Estevão Bresolin <estevao.bresolin@wecode.digital>
2023-11-13 14:45:44 -0300

    setores

---
+++ src/pages/OQueFazemos/Mobile/Setores/index.jsx
@@ -0,0 +1,12 @@
+import SetoresImg from '../../../../assets/imagens/fotos/setores.svg'
+import './sass/Setores.scss'
+const Setores = () => {
+    return (
+        <div className='setores__container'>
+            <p className="container__text1">Aplicamos nossa metodologia em todos os setores promovendo o aculturamento digital e garantindo sinergia entre as áreas da empresa.</p>
+            <img src={SetoresImg} alt="" />
+
+        </div>
+    )
+}
+export default Setores
---
+++ src/pages/OQueFazemos/Mobile/index.jsx
@@ -0,0 +1,14 @@
+import SessaoOQueFazemos from '../../../components/SessaoOQueFazemos/Mobile'
+import Setores from './Setores'
+
+const OqueFazemosMobile = () => {
+    return (
+        <div className='oque-fazemos-mobile'>
+         <SessaoOQueFazemos/>
+         <Setores/>
+            
+        </div>
+    )
+}
+
+export default OqueFazemosMobile
