df396b30bf5620269a0c3ecccbab615f34c66436
Estevão Bresolin <estevao.bresolin@wecode.digital>
2023-11-14 11:13:03 -0300

    ADD nossos clientes desk

---
+++ src/pages/NossosClientes/Desk/SecaoNossosClientesDesk/index.jsx
@@ -0,0 +1,29 @@
+import './sass/style.scss'
+import { logoClientesColorido } from '../../clientes.js'
+
+
+const SessaoNossosClientesDesk = () => {
+    
+    return (
+        <div className="nossos-clientes__container_desk">
+            <div className="nossos-clientes__header">
+                <h4 className="header__title2">NOSSOS CLIENTES</h4>
+                <h3 className="header__title1">Quem <span className='text-highlight'>confia</span>  em nosso trabalho e <br /> <span className='text-highlight'>atesta</span> nossa qualidade</h3>
+                <p className="header__text1">Somos um grupo que oferece diferentes soluções para diferentes necessidades, dentro <br/> do ecossistema do mercado digital.</p>
+            </div>
+
+            <div className='nossos-clientes__body'>
+                
+                {logoClientesColorido.map((cliente) => (
+                    <a className='logo__clientes' href={cliente.link} key={cliente.id} target='_blank'>
+                        <img src={cliente.src} ></img>
+                    </a>
+                   
+                ))}
+               
+            </div>
+        </div>
+    )
+}
+
+export default SessaoNossosClientesDesk
